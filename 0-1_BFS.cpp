7a5f97f7a9d673a113c55a439892890f2cf59cc7
anuranjanpandey <anuranjanpandey5@gmail.com>
2020-10-04 23:58:08 +0530

    Updated 0-1_BFS.cpp

--- 0-1_BFS.cpp
+++ 0-1_BFS.cpp
@@ -20,65 +20,68 @@ const int maxn = (int)(1e5+7);
 
 // weight is either  0 or 1.
 
-
 vector<pii> adj[maxn];
-vector<int> dis(maxn ,inf);
+vector<int> dis(maxn, inf);
 void zobfs(int start)
 {
-    dis[start] =0 ;
+    dis[start] = 0;
     deque<int> q;
     q.push_back(start);
-    while(!q.empty())
+    while (!q.empty())
     {
         int cur = q.front();
         q.pop_front();
 
-        for(auto it : adj[cur])
+        for (auto it : adj[cur])
         {
-            int x = it.F , len = it.S;
-            if(dis[x] > dis[cur] + len)
+            int x = it.F;
+            int len = it.S;
+            if (dis[x] > dis[cur] + len)
             {
                 dis[x] = dis[cur] + len;
-                if(len)
+                if (len)
+                {
                     q.pb(x);
+                }
                 else
+                {
                     q.push_front(x);
+                }
             }
         }
     }
 }
 void solve()
 {
-    int n,i,j,k,m;
-    cin>>n>>m;
-    rep(i,m)
+    int n, i, j, k, m;
+    cin >> n >> m;
+    rep(i, m)
     {
-        int a,b;
-        cin>>a>>b;
-        adj[a].pb({b,0});
-        adj[b].pb({a,1});
+        int a, b;
+        cin >> a >> b;
+        adj[a].pb({b, 0});
+        adj[b].pb({a, 1});
     }
     zobfs(1);
-    cout<<dis[n]<<endl;
-}  
+    cout << dis[n] << endl;
+}
 signed main()
 {
     RAGE;
 
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-        
-    int t=1;
-    //  cin>>t;
-    while(t--)
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t = 1;
+    while (t--)
     {
         solve();
     }
 
-    #ifndef ONLINE_JUDGE
-        cout<<"\nTime Elapsed: " << 1.0*clock() / CLOCKS_PER_SEC << " sec\n";
-    #endif
+#ifndef ONLINE_JUDGE
+    cout << "\nTime Elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " sec\n";
+#endif
     return 0;
 }
